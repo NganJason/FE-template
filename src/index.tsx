055dec26802155729b428fc5f8d03a5fe4ce32c7
jason.ngan <jason.ngan@shopee.com>
2022-08-21 18:09:40 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
+import { ConfigProvider as AntDConfigProvider } from "antd";
+import "antd/dist/antd.variable.css";
+
+const queryClient = new QueryClient();
+
+AntDConfigProvider.config({
+  theme: {
+    primaryColor: "#ffc93c",
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AntDConfigProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </AntDConfigProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
